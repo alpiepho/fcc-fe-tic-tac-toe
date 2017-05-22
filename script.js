9628cfc3cd1011d4013df03306f54f6dafb48b77
apiepho <al@Al-iMac2.local>
2017-05-22 07:07:54 -0600

    almost lost a bunch because of moving between computers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-eX = true;
-var players = 1;
-var auto = true;
+
+var givenUseX = true;
+var useX      = true;
+var players   = 1;
+var auto      = true;
 
 var ttt;
 var gameState;
@@ -19,7 +21,35 @@ function styleButtons(b1, b2, state) {
   }
 }
 
+function styleWin() {
+  for (var slot=0; slot<ttt.winSlots.length; slot++) {
+    id = "#val" + ttt.winSlots[slot];
+    $(id).addClass("won");
+  }
+  if (gameState === ttt.WINX)
+    $("#status").text("X wins!!!");
+  else
+    $("#status").text("0 wins!!!");
+  $("#status").addClass("won");
+
+}
+
+function styleDraw() {
+  $("#val11").addClass("draw");
+  $("#val12").addClass("draw");
+  $("#val13").addClass("draw");
+  $("#val21").addClass("draw");
+  $("#val22").addClass("draw");
+  $("#val23").addClass("draw");
+  $("#val31").addClass("draw");
+  $("#val32").addClass("draw");
+  $("#val33").addClass("draw");
+  $("#status").text("draw :(");
+  $("#status").addClass("draw");
+}
+
 function setX(value) {
+  givenUseX = value;
   useX = value;
   styleButtons("#b1", "#b2", useX);
 }
@@ -27,6 +57,16 @@ function setX(value) {
 function setPlayers(value) {
   players = value;
   styleButtons("#b3", "#b4", (players == 1));
+  if (players === 1) {
+    $("#l3").show();
+    $("#b5").show();
+    $("#b6").show();
+  } else {
+    // 2 player needs to hid auto
+    $("#l3").hide();
+    $("#b5").hide();
+    $("#b6").hide();
+  }
 }
 
 function setAuto(value) {
@@ -45,6 +85,11 @@ function clearBoard() {
   $("#val32").text("_");
   $("#val33").text("_");
   $("span").removeClass("won");
+  $("span").removeClass("draw");
+  $("#status").text("game in progess...");
+  $("#status").removeClass("won");
+  $("#status").removeClass("draw");
+  useX = givenUseX;
   ttt.init();
   gameState = ttt.INPLAY;
 }
@@ -52,7 +97,11 @@ function clearBoard() {
 function setCell(slot) {
   if (gameState !== ttt.INPLAY)
     return;
-  
+  if (slot === 0) {
+    useX = !useX;
+    return;
+  }
+
   var id = "#val" + slot;
 
   if (useX) {
@@ -60,44 +109,30 @@ function setCell(slot) {
     ttt.move(ttt.SLOTX, slot);
   }
   else {
-    $(id).text("0");   
+    $(id).text("0");
     ttt.move(ttt.SLOT0, slot);
-    
+
   }
-  
+
   gameState = ttt.check();
   switch (gameState) {
     case ttt.INPLAY:
       break;
     case ttt.WINX:
-     console.log("X Wins!!!");
-      console.log(ttt.winSlots);
-     for (slot=0; slot<ttt.winSlots.length; slot++) {
-        id = "#val" + ttt.winSlots[slot];
-        $(id).addClass("won");
-      }
-      break;
     case ttt.WIN0:
-      console.log("0 Wins!!!");
-      for (slot=0; slot<ttt.winSlots.length; slot++) {
-        id = "#val" + ttt.winSlots[slot];
-        $(id).addClass("won");
-      }
+      styleWin();
       break;
-    case this.DRAW:
-       console.log("Draw!!!");
+    case ttt.DRAW:
+      styleDraw();
       break;
   }
-  
-  if (!auto && players == 2) {
-    useX = !useX;
-  }
+  useX = !useX;
 }
 
 $(document).ready(function() {
   ttt = new TicTacToe();
   clearBoard();
-  
+
   $("#b1").click(function() { setX(true); });
   $("#b2").click(function() { setX(false); });
   setX(useX);
@@ -110,8 +145,19 @@ $(document).ready(function() {
   $("#b7").click(function() { clearBoard(); });
   $(".cell").click(function(e) {
     // e.target.id could be cellNN or valNN, get NN
-    var slot = Number.parseInt(e.target.id.replace("cell", "").replace("val", ""));
+    var slot;
+    slot = e.target.id.replace("cell", "").replace("val", "");
+    slot = Number.parseInt(slot);
     setCell(slot);
+
+    if (players === 1) {
+      if (auto)
+        slot = ttt.autoMove( (useX ? ttt.SLOTX : ttt.SLOT0) );
+      else
+        slot = ttt.randomMove( (useX ? ttt.SLOTX : ttt.SLOT0) );
+      setCell(slot);
+    }
+
   });
 });
 
@@ -120,39 +166,44 @@ $(document).ready(function() {
 
 
 
-
+// class for the Tic Tac Toe Game
 class TicTacToe {
   constructor() {
     // slot values weighted for fast win determination
     this.SLOT_ = 0;
     this.SLOTX = 1;
     this.SLOT0 = 5;
-    
+
     this.INPLAY = 0;
     this.WINX   = 1;
     this.WIN0   = 2;
     this.DRAW   = 3;
     this.winSlots = [];
-      
+    this.moves = 0;
+
     this.init();
   }
 
+  // iniialize the board
   init () {
-    this.slots = [ 
+    this.slots = [
       [this.SLOT_,this.SLOT_,this.SLOT_],
       [this.SLOT_,this.SLOT_,this.SLOT_],
       [this.SLOT_,this.SLOT_,this.SLOT_]
-    ];   
+    ];
     this.winSlots = [];
+    this.moves = 0;
   }
-  
+
+  // process a move
   move(who, slot) {
     var x = (Number.parseInt(slot / 10)) -1;
     var y = (slot % 10) -1;
-    console.log(x + "," + y);
     this.slots[x][y] = who;
+    this.moves++;
   }
-  
+
+  // check the board for win/draw
   check() {
     var result = this.INPLAY;
     if (result === this.INPLAY) {
@@ -167,14 +218,14 @@ class TicTacToe {
         this.winSlots = [11, 21, 31];
       if ((this.slots[0][1] + this.slots[1][1] + this.slots[2][1]) === 3)
         this.winSlots = [12, 22, 32];
-      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 3) 
+      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 3)
         this.winSlots = [13, 23, 33];
       if ((this.slots[0][0] + this.slots[1][1] + this.slots[2][2]) === 3)
         this.winSlots = [11, 22, 33];
       if ((this.slots[0][2] + this.slots[1][1] + this.slots[2][0]) === 3)
         this.winSlots = [13, 22, 31];
       if (this.winSlots.length > 0)
-        result = this.WINX;        
+        result = this.WINX;
     }
     if (result === this.INPLAY) {
       this.winSlots = [];
@@ -188,14 +239,14 @@ class TicTacToe {
         this.winSlots = [11, 21, 31];
       if ((this.slots[0][1] + this.slots[1][1] + this.slots[2][1]) === 15)
         this.winSlots = [12, 22, 32];
-      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 15) 
+      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 15)
         this.winSlots = [13, 23, 33];
       if ((this.slots[0][0] + this.slots[1][1] + this.slots[2][2]) === 15)
         this.winSlots = [11, 22, 33];
       if ((this.slots[0][2] + this.slots[1][1] + this.slots[2][0]) === 15)
         this.winSlots = [13, 22, 31];
       if (this.winSlots.length > 0)
-        result = this.WIN0;        
+        result = this.WIN0;
     }
     if (result === this.INPLAY) {
       var done = true;
@@ -210,6 +261,107 @@ class TicTacToe {
     }
     return result;
   }
-}
+
+  // NOTE: these could be another class, randomPlayer/autoPlayer
+  mustBlock(currentXor0) {
+    var result = false;
+    this.winSlots = [];
+    if (currentXor0 === this.SLOT0) {
+      // if we are currently 0, check for any near wins of X
+      if ((this.slots[0][0] + this.slots[0][1] + this.slots[0][2]) === 2)
+        this.winSlots = [11, 12, 13];
+      if ((this.slots[1][0] + this.slots[1][1] + this.slots[1][2]) === 2)
+        this.winSlots = [21, 22, 23];
+      if ((this.slots[2][0] + this.slots[2][1] + this.slots[2][2]) === 2)
+        this.winSlots = [31, 32, 33];
+      if ((this.slots[0][0] + this.slots[1][0] + this.slots[2][0]) === 2)
+        this.winSlots = [11, 21, 31];
+      if ((this.slots[0][1] + this.slots[1][1] + this.slots[2][1]) === 2)
+        this.winSlots = [12, 22, 32];
+      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 2)
+        this.winSlots = [13, 23, 33];
+      if ((this.slots[0][0] + this.slots[1][1] + this.slots[2][2]) === 2)
+        this.winSlots = [11, 22, 33];
+      if ((this.slots[0][2] + this.slots[1][1] + this.slots[2][0]) === 2)
+        this.winSlots = [13, 22, 31];
+     }
+    if (currentXor0 === this.SLOTX) {
+      if ((this.slots[0][0] + this.slots[0][1] + this.slots[0][2]) === 10)
+        this.winSlots = [11, 12, 13];
+      if ((this.slots[1][0] + this.slots[1][1] + this.slots[1][2]) === 10)
+        this.winSlots = [21, 22, 23];
+      if ((this.slots[2][0] + this.slots[2][1] + this.slots[2][2]) === 10)
+        this.winSlots = [31, 32, 33];
+      if ((this.slots[0][0] + this.slots[1][0] + this.slots[2][0]) === 10)
+        this.winSlots = [11, 21, 31];
+      if ((this.slots[0][1] + this.slots[1][1] + this.slots[2][1]) === 10)
+        this.winSlots = [12, 22, 32];
+      if ((this.slots[0][2] + this.slots[1][2] + this.slots[2][2]) === 10)
+        this.winSlots = [13, 23, 33];
+      if ((this.slots[0][0] + this.slots[1][1] + this.slots[2][2]) === 10)
+        this.winSlots = [11, 22, 33];
+      if ((this.slots[0][2] + this.slots[1][1] + this.slots[2][0]) === 10)
+        this.winSlots = [13, 22, 31];
+    }
+    if (this.winSlots.length > 0) {
+      // find to the one empty slot
+      var slot;
+      for (var index=0; index < this.winSlots.length; index++) {
+        var slot = this.winSlots[index];
+        var x = (Number.parseInt(slot / 10)) -1;
+        var y = (slot % 10) -1;
+        if (this.slots[x][y] == this.SLOT_)
+          break;
+      }
+      this.winSlots = [ slot ];
+      result = true;
+     }
+
+    return result;
+  }
+
+  // NOTE: this could be another class, randomPlayer
+  randomMove(currentXor0) {
+    if (this.check() !== this.INPLAY)
+      return 0;
+
+    // first check if there is a must block
+    if (this.mustBlock(currentXor0))
+      return ttt.winSlots[0];
+
+    // ok, not random, just first open slot
+    for (var x=0; x<3; x++) {
+      for (var y=0; y<3; y++) {
+        if (ttt.slots[x][y] == this.SLOT_) {
+          var slot = (x+1)*10 + (y+1);
+          return slot;
+        }
+      }
+    }
+    return 0;
+  }
 
 
+  // NOTE: this could be another class, autoPlayer
+  autoMove(currentXor0) {
+    if (this.check() !== this.INPLAY)
+      return 0;
+
+    // first check if there is a must block
+    if (this.mustBlock(currentXor0))
+      return ttt.winSlots[0];
+/*
+    // ok, not random, just first open slot
+    for (var x=0; x<3; x++) {
+      for (var y=0; y<3; y++) {
+        if (ttt.slots[x][y] == this.SLOT_) {
+          var slot = (x+1)*10 + (y+1);
+          return slot;
+        }
+      }
+    }
+    */
+    return 0;
+  }
+
+} // class TicTacToe
